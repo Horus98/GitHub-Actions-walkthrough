fce3c4affacae9ce203c04f05dad3de978faf2d7
Maxi Montenegro <maximontenegro512@gmail.com>
2022-07-10 17:46:22 -0300

    :fix: add port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const port = 4000;
+const port = process.env.port ||4000;
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
