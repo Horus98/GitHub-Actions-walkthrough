8da6b90bef6fec7d74e43aefa273418561f779da
Maxi Montenegro <maximontenegro512@gmail.com>
2022-07-15 22:12:01 -0300

    Text changed

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (_req, res) => {
-  res.send('Express + TypeScript Serverrr');
+  res.send('Express + TypeScript Server');
 });
 
 console.log('testing....');
