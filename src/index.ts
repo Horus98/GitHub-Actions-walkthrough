c093e1725c749c1a3333d074dbe31aa85f25c612
Maxi Montenegro <maximontenegro512@gmail.com>
2022-07-15 22:24:50 -0300

    :fix: Add exclamation

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (_req, res) => {
-  res.send('Express + TypeScript Server');
+  res.send('Express + TypeScript Server!');
 });
 
 console.log('testing....');
