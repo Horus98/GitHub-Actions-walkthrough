462da74867ea445160d94d1192fb1d94d29cbaf4
Maxi Montenegro <maximontenegro512@gmail.com>
2022-07-17 20:43:09 -0300

    Add service

--- src/repositories/broker/services/AccuWeatherService.ts
+++ src/repositories/broker/services/AccuWeatherService.ts
@@ -1,9 +1,11 @@
-class AccuWeatherService {
-    constructor(){
-    }
+import axios from 'axios';
 
-    async getCurrentWeather(){
-        return '';
+class AccuWeatherService {
+
+    API_KEY = 'Vpxn597i5RRSyJl4cInNuoLqHQpBLS4U';
+
+    getCurrentWeather(): Promise<any> {
+        return axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/2931?apikey=${this.API_KEY}&language=en-US&details=true&metric=true`);
     }
 }
 
