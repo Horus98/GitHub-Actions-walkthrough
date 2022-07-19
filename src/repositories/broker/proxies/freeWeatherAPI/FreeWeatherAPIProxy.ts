a00c75216813910a8a960669b07c83fc956277dc
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2022-07-19 00:44:03 -0300

    add getWeatherResponse function

---
+++ src/repositories/broker/proxies/freeWeatherAPI/FreeWeatherAPIProxy.ts
@@ -0,0 +1,23 @@
+import Proxy from '../Proxy';
+import WeatherResponse from '../../WeatherResponse';
+import FreeWeatherAPIService from '../../services/FreeWeatherAPIService';
+import FreeWeatherAPIResponseBuilder from './FreeWeatherAPIResponseBuilder';
+import { injectable } from 'tsyringe';
+
+@injectable()
+class FreeWeatherAPIProxy implements Proxy {
+    service: FreeWeatherAPIService;
+    responseBuilder: FreeWeatherAPIResponseBuilder;
+
+    constructor(service: FreeWeatherAPIService, responseBuilder: FreeWeatherAPIResponseBuilder) {
+        this.service = service;
+        this.responseBuilder = responseBuilder;
+    }
+
+    async getWeatherResponse(cityName: string): Promise<WeatherResponse> {
+        const currentWetherResponse = await this.service.getCurrentWeather(cityName);
+        return this.responseBuilder.buildWeatherResponse(currentWetherResponse.data, cityName);
+    }
+}
+
+export default FreeWeatherAPIProxy;
