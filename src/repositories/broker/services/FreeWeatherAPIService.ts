c3f9669ca153c1cccf607be3e1fe949bb9c45a7c
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2022-07-19 00:16:57 -0300

    add getCurrentWeather function

---
+++ src/repositories/broker/services/FreeWeatherAPIService.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+class FreeWeatherAPIService {
+
+    API_KEY = 'b4067c5cbc3b4b119ce222559222606';
+
+    async getCurrentWeather(cityName: string): Promise<any> {
+        return axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${cityName}&days=1&aqi=no&alerts=no`);
+    }
+}
+
+export default FreeWeatherAPIService;
