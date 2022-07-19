73e0caf417c8b28ea79c3db967d80fe1196af157
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2022-07-19 01:17:44 -0300

    :ready: API lista

--- src/repositories/broker/services/FreeWeatherAPIService.ts
+++ src/repositories/broker/services/FreeWeatherAPIService.ts
@@ -5,7 +5,7 @@ class FreeWeatherAPIService {
     API_KEY = 'b4067c5cbc3b4b119ce222559222606';
 
     async getCurrentWeather(cityName: string): Promise<any> {
-        return axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${cityName}&days=1&aqi=no&alerts=no`);
+        return axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${cityName}&days=1&alerts=no&aqi=no`);
     }
 }
 
